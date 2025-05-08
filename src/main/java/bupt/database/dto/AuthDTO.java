78c478e0d4967555e5b0af72ef6cad962247c0cf
chenhhhh <chenhhhh1717@gmail.com>
2025-05-08 20:12:50 +0800

    feat: 用户管理完成

--- src/main/java/bupt/database/dto/AuthDTO.java
+++ src/main/java/bupt/database/dto/AuthDTO.java
@@ -1,8 +1,12 @@
 package bupt.database.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthDTO {
     private String username;
     private String password;
